189b46c45dd362afe15aaa350a2dc7d2fb1de649
Marcoslsl <marcosviniciuseg@outlook.com>
2023-08-27 23:40:45 -0300

    feat: add typeorm module on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local', '.env.development'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
     UserModule,
   ],
